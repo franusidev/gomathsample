755a48d19bdf3268ebe769f0073895fbe516e988
Francisco Nunez Sierra <franusidev@gmail.com>
2026-05-03 11:57:33 +0200

    doc: add basic documentation

--- gomathsample.go
+++ gomathsample.go
@@ -1,5 +1,8 @@
+// This module contains simple math operations
 package gomathsample
 
+// A function to add to numbers and return the results
+// You can learn more about this operation at https://www.mathsisfun.com/numbers/addition.html
 func Add(a int,b int) int{
 	return a+b
 }
